ba928a807d6ea9efc5e80ba3c73cca5014de7874
abdotamer <btgawad@yahoo.com>
2022-03-28 11:05:40 +0800

    Final Code for the project

--- Blinking_LED/Blinking_LED.ino
+++ Blinking_LED/Blinking_LED.ino
@@ -8,10 +8,35 @@ static const int LedPin = 12;
 
 void LedToggle1(void * par)
 {
+  while(1)
+  {
   digitalWrite(LedPin, HIGH);
-  vTaskDelay(50 / portTICK_PERIOD_MS);
+  vTaskDelay(500 / portTICK_PERIOD_MS);
   digitalWrite(LedPin, LOW);
-  vTaskDelay(50 / portTICK_PERIOD_MS);
+  vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+void LedToggle2(void * par)
+{
+  while(1)
+  {
+  digitalWrite(LedPin, HIGH);
+  vTaskDelay(210 / portTICK_PERIOD_MS);
+  digitalWrite(LedPin, LOW);
+  vTaskDelay(210 / portTICK_PERIOD_MS);
+  }
+}
+
+void LedToggle3(void * par)
+{
+  while(1)
+  {
+  digitalWrite(LedPin, HIGH);
+  vTaskDelay(180 / portTICK_PERIOD_MS);
+  digitalWrite(LedPin, LOW);
+  vTaskDelay(180 / portTICK_PERIOD_MS);
+  }
 }
 
 void setup() {
@@ -19,6 +44,8 @@ void setup() {
   pinMode(LedPin, OUTPUT);
 
   xTaskCreatePinnedToCore(LedToggle1, "Toggle Led 1", 1024, NULL, 1, NULL, app_cpu);
+  xTaskCreatePinnedToCore(LedToggle2, "Toggle Led 2", 1024, NULL, 2, NULL, app_cpu);
+  xTaskCreatePinnedToCore(LedToggle3, "Toggle Led 3", 1024, NULL, 3, NULL, app_cpu);
 }
 
 void loop() {
